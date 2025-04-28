3c35d6e6ce18f50ba2a9012874dd89c75e8851f0
Rolland_Dev <rolland.h.dev@gmail.com>
2025-04-28 06:20:23 +0200

    Commit initial

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const hambMenuLink = document.querySelector(".nav-link");
 hambMenu.addEventListener("click", () => {
   if (hambMenuLink.style.opacity !== "1") {
     hambMenuLink.style.opacity = "1";
+    hambMenuLink.style.right = "0";
   } else {
     hambMenuLink.style.opacity = "0";
+    hambMenuLink.style.right = "-100px";
   }
 });
